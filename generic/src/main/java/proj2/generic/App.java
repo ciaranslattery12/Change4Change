3669eeb1f89522df2595f8a636777f4f1debeefb
Ciaran Slattery <ciaranslattery12@gmail.com>
2017-08-15 15:55:52 -0400

    message

---
+++ generic/src/main/java/proj2/generic/App.java
@@ -0,0 +1,13 @@
+package proj2.generic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
