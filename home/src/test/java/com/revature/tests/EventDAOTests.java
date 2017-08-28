ba54b90ba5d603d04d5c1f5bcea7417d06e9deed
Ciaran <ciaranslattery12@gmail.com>
2017-08-28 17:08:30 -0400

    updated event tests

--- home/src/test/java/com/revature/tests/EventDAOTests.java
+++ home/src/test/java/com/revature/tests/EventDAOTests.java
@@ -40,7 +40,7 @@ public class EventDAOTests extends ChangeForChangeTests {
 		context = new ClassPathXmlApplicationContext("dao-beans.xml");
 	}
 	
-	//@Ignore
+	@Ignore
 	@Test
 	public void createTest() throws ParseException{
 		
@@ -51,9 +51,6 @@ public class EventDAOTests extends ChangeForChangeTests {
 		Date date = dateFormat.parse("01/09/2017");
 		long time = date.getTime();
 
-		
-			//eventDAOManager = context.getBean(EventDAOManager.class);
-		
 		eventDAOManager = (EventDAOManager) context.getBean("eventDAO");
 		
 		// instantiate custom types
@@ -70,4 +67,45 @@ public class EventDAOTests extends ChangeForChangeTests {
 
 		assertEquals(++rowCount, newRowCount);
 	}
+	
+	@Ignore
+	@Test
+	public void updateTest() {
+		
+		/*eventDAOManager = (EventDAOManager) context.getBean("eventDAO");
+		
+		// instantiate custom types
+		EventType type = new EventType(2, "FUNDRAISER");
+		EventStatus status = new EventStatus(1, "UPCOMING");
+		UsersRole role = new UsersRole(1, "ADMIN");
+		Users user = new Users(142, "Patrick", "Muldoon", "patrickM" , "password", "patrick@example.com", role);
+		
+		Events event = new Events(250, new Timestamp(time), "Diabetes Walk", type, user, status);
+		eventDAOManager.update(event);*/
+	}
+	
+	@Ignore
+	@Test
+	public void deleteTest() {
+		
+	}
+	
+	@Ignore
+	@Test
+	public void findByIdTest() {
+		
+	}
+	
+	@Ignore
+	@Test
+	public void findAllTest() {
+		
+	}
+	
+	
+	
+	
+	
+	
+	
 }
