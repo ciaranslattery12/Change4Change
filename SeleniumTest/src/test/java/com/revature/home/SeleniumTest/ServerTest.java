72a65b3f31d7953f610d9eb748d2f5a8668c8d93
Patrick <patrickmuldoon20@yahoo.com>
2017-09-15 13:13:03 -0400

    Trying to use HTMLUnitDriver again

--- SeleniumTest/src/test/java/com/revature/home/SeleniumTest/ServerTest.java
+++ SeleniumTest/src/test/java/com/revature/home/SeleniumTest/ServerTest.java
@@ -6,18 +6,16 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.phantomjs.PhantomJSDriver;
-import org.openqa.selenium.phantomjs.PhantomJSDriverService;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
 
 public class ServerTest {
 
 	   @Test
 	   public void openBlazeMeterTest() {
-		   DesiredCapabilities caps = new DesiredCapabilities();
-		   caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "/home/ec2-user/selenium-server/phantomjs-1.9.6-linux-x86_64/bin/phantomjs");
-		   WebDriver driver = new PhantomJSDriver(caps);
-	       driver.get("http://13.59.180.148:8080/index.html");
+		   WebDriver driver = new HtmlUnitDriver(BrowserVersion.FIREFOX_45);
+	       driver.get("http://google.com");
 	       WebElement title = driver.findElement(By.xpath("//title"));
 	       System.out.println("********************************************************");
 	       System.out.println("*" + title.getAttribute("text") + "*");
@@ -25,21 +23,19 @@ public class ServerTest {
 	       driver.quit();
 	   }
 	   
-	   @Test
-		public void loginAsAdmin(){
-		   DesiredCapabilities caps = new DesiredCapabilities();
-		   caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "/usr/bin/phantomjs");
-		   WebDriver driver = new PhantomJSDriver(caps);
-			driver.get("http://13.59.180.148:8080/index.html#/login");
-			//sign in user with admin privaleges
-			driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/form/div[1]/input")).sendKeys("pMuldoon20");
-			driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/form/div[2]/input[1]")).sendKeys("password!");
-			driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/form/div[2]/input[2]")).click();
-			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-			//find elements that should be on home page after login
-			//driver.findElement(By.cssSelector("body > div:nth-child(2) > div > div > div > div:nth-child(1) > h1"));
-			//driver.findElement(By.cssSelector("#myCarousel > div"));
-			driver.findElement(By.xpath("//*[@id='bs-example-navbar-collapse-9']/ul[2]/li[1]/a")).click();
-		}
+//	   @Test
+//		public void loginAsAdmin(){
+//		   WebDriver driver = new FirefoxDriver();
+//			driver.get("http://13.59.180.148:8080/index.html#/login");
+//			//sign in user with admin privaleges
+//			driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/form/div[1]/input")).sendKeys("pMuldoon20");
+//			driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/form/div[2]/input[1]")).sendKeys("password!");
+//			driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/form/div[2]/input[2]")).click();
+//			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//			//find elements that should be on home page after login
+//			//driver.findElement(By.cssSelector("body > div:nth-child(2) > div > div > div > div:nth-child(1) > h1"));
+//			//driver.findElement(By.cssSelector("#myCarousel > div"));
+//			//driver.findElement(By.xpath("//*[@id='bs-example-navbar-collapse-9']/ul[2]/li[1]/a")).click();
+//		}
 	
 }
