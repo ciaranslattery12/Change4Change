81406290aecac5069a5ebb1d2750ab8a56f6f1cf
Patrick <patrickmuldoon20@yahoo.com>
2017-09-19 13:38:22 -0400

    Testing PhantomJS

--- SeleniumTest/src/test/java/com/revature/home/SeleniumTest/ServerTest.java
+++ SeleniumTest/src/test/java/com/revature/home/SeleniumTest/ServerTest.java
@@ -5,13 +5,17 @@ import static org.junit.Assert.*;
 import java.time.zone.ZoneOffsetTransitionRule.TimeDefinition;
 import java.util.concurrent.TimeUnit;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.support.FindBy;
 
 import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.javascript.host.Element;
 
 public class ServerTest {
 
@@ -26,22 +30,27 @@ public class ServerTest {
 //	       System.out.println("********************************************************");
 //	       driver.quit();
 //	   }
-	   
+
 	   @Test
-	   public void openC4C(){
-		   WebDriver driver = new HtmlUnitDriver(BrowserVersion.CHROME,true);
+	   public void openC4C() throws InterruptedException{
+		   WebDriver driver = new PhantomJSDriver();
 		   driver.navigate().to("http://13.59.180.148:8080/index.html");
+		   driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		   System.out.println(driver.getCurrentUrl());
 		   WebElement title = driver.findElement(By.xpath("//title"));
 	       System.out.println("********************************************************");
 	       System.out.println("*" + title.getAttribute("text") + "*");
 	       System.out.println("********************************************************");
+//	       driver.findElement(By.xpath("/html/body/div/ui-view/nav/div/ul[2]/li[2]/a")).click();
+//	       System.out.println(driver.getCurrentUrl());
+//	       driver.findElement(By.xpath("//*[@id='manage']/div[1]/div/div/ul/li[3]/a"));
+//	       assertEquals("http://localhost:8080/caliber#/vp/manage", driver.getCurrentUrl());
 	       driver.quit();
 	   }
-	   
+
 	   @Test
 	   public void loginAsAdmin(){
-		   WebDriver driver = new HtmlUnitDriver(BrowserVersion.CHROME, true);
+		   WebDriver driver = new PhantomJSDriver();
 		   driver.get("http://13.59.180.148:8080/index.html#/login");
 		   System.out.println(driver.getCurrentUrl());
 		   driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -51,5 +60,5 @@ public class ServerTest {
 		   System.out.println("Success");
 	   }
 
-	
+
 }
